5a5d1ff63ca6aef640e2dc779260ea01708f371c
scavo <157555028+awoyaledolapo@users.noreply.github.com>
2025-03-20 09:48:24 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> 
+    <BrowserRouter base:"/dashboard"> 
       <App />
     </BrowserRouter>
   </StrictMode>,
